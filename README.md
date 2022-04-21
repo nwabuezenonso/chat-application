eb04496af85068a7fdec3099f97bdb238c430b0a
stephen <nwabuezesc@gmail.com>
2022-04-21 07:14:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Introduction
+ A full Realtime Chat Application using React on the front end, with NodeJS + Socket.io web socket library on the back end.
+
+ 
---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter,Routes ,Route} from 'react-router-dom'
+import Chat from './components/Chat'
+import Join from './components/Join'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Join />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
